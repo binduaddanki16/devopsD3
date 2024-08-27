9d3390c1179687dada59f3f1c182ff4bdb67afdb
AdmiN <AdmiN@ITWS000>
2024-08-27 11:10:09 +0530

    commit

--- devopsD3/src/devopsD3/kimjonun.java
+++ devopsD3/src/devopsD3/kimjonun.java
@@ -5,6 +5,8 @@ public class kimjonun {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("I am Rajendra this is to transfer the file");
+System.out.println("I am Rajendra this is to transfer the file");
+
 	}
 
 }
