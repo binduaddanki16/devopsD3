91407100c036d664595597a3c458e05474a9fd9e
AdmiN <AdmiN@ITWS000>
2024-08-27 10:33:31 +0530

    change

---
+++ devopsD3/src/devopsD3/kimjonun.java
@@ -0,0 +1,10 @@
+package devopsD3;
+
+public class kimjonun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am Rajendra this is to transfer the file");
+	}
+
+}
